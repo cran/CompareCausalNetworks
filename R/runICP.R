c8c032a588138ba379f08a73e42fd3e3304448c9
Christina Heinze <heinze@stat.math.ethz.ch>
2015-10-07 09:49:12 +0000

    version 0.1.4

--- R/runICP.R
+++ R/runICP.R
@@ -3,7 +3,7 @@ runICP <- function(X, environment, interventions, parentsOf, alpha,
                    setOptions, verbose, result){
   
   # additional options for ICP
-  optionsList <- list("gof" = 0.1, "test"="approximate", 
+  optionsList <- list( "test"="normal", 
                       "selection"="lasso", "maxNoVariables"=7, 
                       "maxNoVariablesSimult"=3, "maxNoObs"=200, 
                       "stopIfEmpty"=TRUE, "selfselect"=NULL)
@@ -13,16 +13,17 @@ runICP <- function(X, environment, interventions, parentsOf, alpha,
                                optionsToSet = setOptions)
   
   
-  if(all((1:ncol(X)) %in% parentsOf) & is.null(interventions)) 
+  if(all((1:ncol(X)) %in% parentsOf) & is.null(interventions) & excludeTargetInterventions) 
     warning(
      "ICP requires that no interventions occured on the target variables. In the 
      current function call 
      (a) all variables are considered as target variables (parentsOf=1:ncol(X)) and 
      (b) the interventions are equal to NULL (and can thus not be removed for 
-     each variables). 
+     each variable).
+
      The results are likely to be misleading. Either target just specific 
      variables by specifying 'parentsOf' or add the list where interventons 
-     occured (using argument 'interventions'.")
+     occured (using argument 'interventions').\n")
   
   # use data from different environments/interventions
   if(excludeTargetInterventions & !is.null(interventions)){
@@ -48,10 +49,11 @@ runICP <- function(X, environment, interventions, parentsOf, alpha,
     removeVar <- parentsOf[k]
     
     if(!is.null(variableSelMat) & is.null(optionsList$selfselect)){
-      if(ncol(variableSelMat)==length(parentsOf)) 
+      if(ncol(variableSelMat)==length(parentsOf)){
         selc <- k 
-      else 
+      }else{
         selc <- which( (1:ncol(X)) == parentsOf[k])
+    }
       removeVar <- 
         unique(c(removeVar,which( !variableSelMat[,selc] )))
     }else{
@@ -83,7 +85,6 @@ runICP <- function(X, environment, interventions, parentsOf, alpha,
        ExpInd=environment[allobs], 
        showAcceptedSets=FALSE,
        showCompletion=FALSE, alpha=alpha, 
-       gof=optionsList$gof, 
        test=optionsList$test, 
        selection=optionsList$selection, 
        maxNoVariables=optionsList$maxNoVariables, 
@@ -96,7 +97,7 @@ runICP <- function(X, environment, interventions, parentsOf, alpha,
     if(confBound)
       attr(result[[k]],"coefficients") <- 
       res$maximinCoefficients[which(res$maximinCoefficients !=0)]
-  }
+}
     
   result
 }
